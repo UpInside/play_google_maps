56213ca92fcc088427017edc4dd62b66ddced211
Robson V. Leite <cursos@upinside.com.br>
2017-12-08 16:03:58 -0200

    Class documentation

--- Source/Models/GoogleMaps.php
+++ Source/Models/GoogleMaps.php
@@ -2,6 +2,14 @@
 
 namespace Source\Models;
 
+/**
+ * GoogleMaos: Classe de integração com o Google Maps geocode criada na aula
+ * Consumo e personalização do Google Maps + Geocode via API
+ * 
+ * @author Robson V. Leite <robson@upinside.com.br>
+ * @link https://www.upinside.com.br/ Saiba mais
+ * @copyright (c) 2017, Robson V. Leite - UPINSIDE TECNOLOGIA
+ */
 class GoogleMaps
 {
 
@@ -18,6 +26,10 @@ class GoogleMaps
         $this->apiUrl = "https://maps.googleapis.com/maps/api/geocode/json?key={$this->apiKey}&sensor=false&language=pt-BR";
     }
 
+    /**
+     * setAddress: Define qual endereço buscar
+     * @param string $params Endereço (Florianópolis | Florianópolis Campeche | Rua XPTO)
+     */
     public function setAddress($params)
     {
         $this->params = str_replace(" ", "+", $params);
@@ -25,23 +37,38 @@ class GoogleMaps
         return $this;
     }
 
+    /**
+     * getAddress: Retorna o endereço escrito
+     * @return string Florianópolis = Florianópolis, Santa Catarina, Brasil
+     */
     public function getAddress()
     {
         $this->address = $this->callback->results[0]->formatted_address;
         return $this->address;
     }
 
+    /**
+     * getLatLng: Retorna latitude e longitude do endereço
+     * @return object ->lat and ->lng
+     */
     public function getLatLng()
     {
         $this->latLng = $this->callback->results[0]->geometry->location;
         return $this->latLng;
     }
 
+    /**
+     * getCallback: Retorna o objeto completo do endereço
+     * @return object Todos os atributos.
+     */
     public function getCallback()
     {
         return $this->callback;
     }
 
+    /**
+     * Efetua uma comunicação via HTTP GET
+     */
     private function get()
     {
         $ch = curl_init();
